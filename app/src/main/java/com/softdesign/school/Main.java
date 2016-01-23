69f6eab8f81a4a8742a89ac02fb319c3875567ba
YuliaRada <radistkatlt@gmail.com>
2016-01-24 01:00:41 +0400

    Initial commit

---
+++ app/src/main/java/com/softdesign/school/Main.java
@@ -0,0 +1,18 @@
+package com.softdesign.school;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+   }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/src/main/java/com/softdesign/school/utils/GitKeep.java
@@ -0,0 +1,7 @@
+package com.softdesign.school.utils;
+
+/**
+ * Created by Юлия on 24.01.2016.
+ */
+public class GitKeep {
+}
